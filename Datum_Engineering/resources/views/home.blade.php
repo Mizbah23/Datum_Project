8d3c9158af3543bd66fe7e542ee90f8208d7abde
Mizbah23 <mizbahmed161@gmail.com>
2019-07-22 20:55:22 +0600

    22/07/19

--- Datum_Engineering/resources/views/home.blade.php
+++ Datum_Engineering/resources/views/home.blade.php
@@ -524,8 +524,8 @@
     <script src="js/jquery.diamonds.js"></script>
     <script>
         $(".diamond-grid").diamonds({
-            size : 200, // Size of diamonds in pixels. Both width and height. 
-            gap : 5, // Pixels between each square.
+            size : 230, // Size of diamonds in pixels. Both width and height. 
+            gap : 4, // Pixels between each square.
             hideIncompleteRow : false, // Hide last row if there are not enough items to fill it completely.
             autoRedraw : true, // Auto redraw diamonds when it detects resizing.
             itemSelector : ".itam" // the css selector to use to select diamonds-items.
